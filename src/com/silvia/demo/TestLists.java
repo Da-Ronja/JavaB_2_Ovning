e6ad5efae65ae77545ae5c0c62593c9013058206
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2022-09-29 13:06:42 +0200

    Second assignment done

--- src/com/silvia/demo/TestLists.java
+++ src/com/silvia/demo/TestLists.java
@@ -12,8 +12,24 @@ public class TestLists {
         System.out.println("After initializing scoreList in arrayListMethod " + scoreList);
 
         scoreList.add(5);
+        scoreList.add(8);
+        scoreList.add(2);
+        scoreList.add(6);
 
         System.out.println("After adding to scoreList in arrayListMethod " + scoreList);
+
+        scoreList.remove(0);
+
+        System.out.println("After removing from scoreList in arrayListMethod " + scoreList);
+
+        scoreList.set(1, 7);
+
+        System.out.println("After changing in scoreList in arrayListMethod " + scoreList);
+
+        int numByIndex = scoreList.get(2);
+        System.out.println("Getting from scoreList in arrayListMethod by Variable " + numByIndex);
+        System.out.println("Getting from scoreList in arrayListMethod in s.o.u.t " + scoreList.get(2));
+        System.out.println(" ");
     }
 
     public static void linkedListMethod() {
@@ -21,9 +37,36 @@ public class TestLists {
 
         System.out.println("After initializing scoreList in linkedListMethod " + scoreList);
 
-        scoreList.add(3);
+        /* scoreList.add(3);
+        scoreList.add(8);
+        scoreList.add(9);
+        scoreList.add(6); */
+        scoreList = addLinkedListMethod(3, 8, 9, 6);
 
         System.out.println("After adding to scoreList in linkedListMethod " + scoreList);
+
+        scoreList.remove(3);
+
+        System.out.println("After removing from scoreList in linkedListMethod " + scoreList);
+
+        scoreList.set(1, 5);
+
+        System.out.println("After changing in scoreList in linkedListMethod " + scoreList);
+
+        int numByIndex = scoreList.get(0);
+        System.out.println("Getting from scoreList in linkedListMethod by Variable " + numByIndex);
+        System.out.println("Getting from scoreList in linkedListMethod in s.o.u.t " + scoreList.get(0));
+        System.out.println(" ");
     }
 
+    public static List<Integer> addLinkedListMethod(int x, int y, int z, int w) {
+        List<Integer> scoreList = new LinkedList<>();
+
+        scoreList.add(x);
+        scoreList.add(y);
+        scoreList.add(z);
+        scoreList.add(w);
+
+        return scoreList;
+    }
 }
